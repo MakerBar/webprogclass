a8989fa3f7992cfecda1508f726fad119e4d3462
Jason Biegel <jason@biesel.co>
2015-08-16 10:16:33 -0400

    some updates

--- scripts.js
+++ scripts.js
@@ -1,5 +1,25 @@
 $(function() {
-   $('#products li').click(function(e) {
-       $('#cart').append(e.target);
-   }) 
+    var subtotal = 0.0;
+
+    function updateTotal(price) {
+        subtotal += price;
+
+        var tax = subtotal * .07;
+        var total = subtotal + tax;
+
+        //display the updated figures
+        $('#subtotal').text('$' + (Math.round(subtotal * 100) / 100));
+        $('#tax').text('$' + (Math.round(tax * 100) / 100));
+        $('#total').text('$' + (Math.round(total * 100) / 100));
+    }
+
+    $('#products li').click(function(event) {
+
+       //move the product into the shopping cart
+       $('#cart').append(event.target);
+
+        var price = parseFloat($(event.target).data('price'));
+
+        updateTotal(price);
+    });
 });
